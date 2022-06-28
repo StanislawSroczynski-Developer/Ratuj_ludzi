ca22c2b874c64c7e8b7edbf9aa8c48d7ec41e304
StanleySro <stasiu_sroczynski@tlen.pl>
2022-06-28 16:08:28 +0200

    adding new branch

--- Ratuj_ludzi/MainWindow.xaml.cs
+++ Ratuj_ludzi/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -17,6 +18,7 @@ namespace Ratuj_ludzi
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random random = new Random();
         public MainWindow()
         {
             InitializeComponent();
@@ -24,7 +26,30 @@ namespace Ratuj_ludzi
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            AddEnemy();
+        }
 
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 25, new PropertyPath("(Canvas.Left)"));
+            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 25), random.Next((int)playArea.ActualHeight - 25), new PropertyPath("(Canvas.Top)"));
+            playArea.Children.Add(enemy);
+        }
+
+        private void AnimateEnemy(ContentControl enemy, double from, double to, PropertyPath propertyToAnimate)
+        {
+            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+            };
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, propertyToAnimate);
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
         }
     }
 }
